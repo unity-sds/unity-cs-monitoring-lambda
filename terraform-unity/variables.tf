645d2410b83e7eb45806d7126243e5e558dae148
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2024-05-27 17:29:40 -0700

    Create variables.tf

---
+++ terraform-unity/variables.tf
@@ -0,0 +1,9 @@
+variable "parameter_name" {
+  description = "The name of the SSM parameter"
+  type        = string
+}
+
+variable "parameter_value" {
+  description = "The value of the SSM parameter"
+  type        = string
+}
