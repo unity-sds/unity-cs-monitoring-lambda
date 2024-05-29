7a89db5ee8e07ec7dce5bb699ac85f273f3840d1
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2024-05-28 18:46:16 -0700

    Update outputs.tf

--- terraform-unity/outputs.tf
+++ terraform-unity/outputs.tf
@@ -1,3 +1,4 @@
-output "parameter_name" {
-  value = aws_ssm_parameter.test_parameter.name
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.unity_cs_monitoring_lambda.arn
 }
