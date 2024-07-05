f3eb840c5fc21e364f9f849b695cb51a2be445ac
Brandon Hargitay <141683542+hargitayjpl@users.noreply.github.com>
2024-07-05 10:43:04 -0700

    Update variables.tf

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -7,3 +7,18 @@ variable "project" {
   description = "The project name"
   type        = string
 }
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string
+}
+
+variable "installprefix" {
+  description = "A prefix used for installation."
+  type        = string
+}
+
+variable "deployment_name" {
+  description = "The name of the deployment."
+  type        = string
+}
